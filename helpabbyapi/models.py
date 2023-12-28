965fc64f1607f106d5f0337f1eceea9ec21757d0
t-novia.sari <t-novia.sari@traveloka.com>
2023-12-28 17:33:01 +0700

    register new model myths and foods

--- helpabbyapi/models.py
+++ helpabbyapi/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Myth(models.Model):
+    mythDesc = models.CharField(max_length=1000)
+    factDesc = models.CharField(max_length=1000)
+    source = models.CharField(max_length=100)
+
+class Food(models.Model):
+    foodName = models.CharField(max_length=1000)
+    calories = models.IntegerField()
+    carbs = models.DecimalField(max_digits=1000, decimal_places=1)
+    protein = models.DecimalField(max_digits=1000, decimal_places=1)
+    fat = models.DecimalField(max_digits=1000, decimal_places=1)
